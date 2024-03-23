8f51c3b2a8e95313dc209d5657168b66c2f9cde5
Flaryiest <ericmzuo1@gmail.com>
2024-03-22 19:54:32 -0600

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    mode: "development",
+  entry: './src/index.js',
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: "[id].[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+        inject: "body",
+    }),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
